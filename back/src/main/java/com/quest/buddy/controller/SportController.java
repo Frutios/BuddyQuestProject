d9505c0e8005f61341859aac6fa7ddf14d8fe443
edisonknewgitlab <edisonknew@gmail.com>
2021-06-28 00:17:17 +0200

    Chargement des données events users via fetch JS

--- back/src/main/java/com/quest/buddy/controller/SportController.java
+++ back/src/main/java/com/quest/buddy/controller/SportController.java
@@ -1,8 +1,13 @@
 package com.quest.buddy.controller;
 
+import java.util.Arrays;
+
 import com.quest.buddy.dtos.SportDto;
 import com.quest.buddy.dtos.SportFilters;
+import com.quest.buddy.models.Event;
 import com.quest.buddy.models.Sport;
+import com.quest.buddy.services.MySportService;
+import com.quest.buddy.services.MySportServiceImpl;
 import com.quest.buddy.services.SportServiceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,17 +15,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping({ "/sports"})
 public class SportController {
     
     @Autowired
     private SportServiceImpl sportService;
 
-    @GetMapping({"/sports"})
-    public String displaySports(@ModelAttribute("filters") SportFilters sportFilters,Model model,String filter) {
+    @Autowired
+    private MySportServiceImpl mySportService;
+
+    @GetMapping
+    public String main(@ModelAttribute("filters") SportFilters sportFilters,Model model,String filter) {
     
        Iterable<SportDto> sports;
        
@@ -40,19 +51,16 @@ public class SportController {
         return "views/sports";
     }
     
-    @GetMapping({"/sports/details"})
-    public String displaySport(@RequestParam("id") Long id, Model model) {
+    @GetMapping({"/details"})
+    public String details(@RequestParam("id") Long id, Model model) {
 
         Sport sport = sportService.findById(id);
-
         model.addAttribute("sport", sport.toDto());
-        model.addAttribute("events", sportService.getEventsForSport(id));
-        // model.addAttribute("users", sportService.getUsersForSport(id));
         return "views/sportDetails";
     }
 
-    @PostMapping({"/sports/details"})
-    public String updateSport(@RequestParam("id") Long id,@ModelAttribute("sport") SportDto sportModel,Model model) {
+    @PostMapping({"/details"})
+    public String update(@RequestParam("id") Long id,@ModelAttribute("sport") SportDto sportModel,Model model) {
 
         Sport sport = sportService.findById(sportModel.getId());
         sport.setActive(sportModel.getActive());
@@ -60,9 +68,33 @@ public class SportController {
         sportService.update(sport);
 
         model.addAttribute("sport", sport.toDto());
-        model.addAttribute("events", sportService.getEventsForSport(id));
-        model.addAttribute("users", sportService.getUsersForSport(id));
         return "views/sportDetails";
+    }
+
+    @GetMapping("{details}")
+    public String tab(@PathVariable String details,@RequestParam("id") Long sportId,Model model) {
+
+        Sport sport = sportService.findById(sportId);
+        switch(details){
+            case "events":
+            model.addAttribute("events", sportService.getEventsForSport(sportId));
+            return "/fragments/events";
+            case "users":
+            model.addAttribute("users", mySportService.findUserBySport(sport));
+            return "/fragments/users";
+        }
+       
+        return "error";
+    }
+
+    @GetMapping("/testCall")
+    public String testCall(@PathVariable String tab) {
+        if (Arrays.asList("tab1", "tab2", "tab3")
+                  .contains(tab)) {
+            return "_" + tab;
+        }
+
+        return "empty";
     }
 
 }
--- back/src/main/java/com/quest/buddy/repository/MySportRepository.java
+++ back/src/main/java/com/quest/buddy/repository/MySportRepository.java
@@ -1,11 +1,15 @@
 package com.quest.buddy.repository;
 
 
+import java.util.List;
+
 import com.quest.buddy.models.MySport;
+import com.quest.buddy.models.Sport;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MySportRepository extends CrudRepository<MySport, Long> {
+    public List<MySport> findBySport(Sport sport);
 }
