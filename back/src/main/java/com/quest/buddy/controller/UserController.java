a864ca9f453a48e87fe48d32ae8ec5396ad52122
Benjamin Tourneur <60399610+Frutios@users.noreply.github.com>
2021-06-28 18:16:56 +0200

    modifier un utilisateur

--- back/src/main/java/com/quest/buddy/controller/UserController.java
+++ back/src/main/java/com/quest/buddy/controller/UserController.java
@@ -10,16 +10,19 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.quest.buddy.services.UserServiceImpl;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
+@RequestMapping({ "/users"})
 @Controller
 public class UserController {
 
     @Autowired
     UserServiceImpl userService;
 
-    @PostMapping("/adduser")
+    @PostMapping("/add")
     public String addUser(@ModelAttribute User user,  Model model) {
         model.addAttribute("user", user);
         userService.create(user);
@@ -35,14 +38,33 @@ public class UserController {
     }
 
     
-    @GetMapping("/users")
+    @GetMapping("")
     public String displayUserList(Model model){
         model.addAttribute("users", userService.getAll());
         
         return "views/userlist";
     }
 
+    @GetMapping("/update")
+    public String displayUpdateUser(@RequestParam(name = "id") Long id, Model model){
 
+        model.addAttribute("user", userService.findById(id));
 
+        return "views/userupdate";
+    }
+
+    @PostMapping("/update")
+    public String updateUser(@RequestParam(name = "id") Long id, @ModelAttribute("user") User user, Model model ) {
+        
+        User userDatabase = userService.findById(user.getId());
+
+        userDatabase.setFirstName(user.getFirstName());
+        userDatabase.setLastName(user.getLastName());
+        userDatabase.setPhone(user.getPhone());
+
+        userService.update(userDatabase);
+
+        return "redirect:userlist";
+    }
     
 }
