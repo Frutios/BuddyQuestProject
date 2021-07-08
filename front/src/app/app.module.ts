d53d2a2876571bd04ec46ca4268b650d3b6dec36
Benjamin Tourneur <60399610+Frutios@users.noreply.github.com>
2021-07-08 15:00:08 +0200

    création utilisateur depuis angular

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
