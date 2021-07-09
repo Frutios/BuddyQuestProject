1bfa07d5547d2398057af3a8bd9f0de8536cbc0f
edisonknewgitlab <edisonknew@gmail.com>
2021-07-09 11:22:39 +0200

    Correction de Romain qui fait de la µµµµµ

--- back/src/main/java/com/quest/buddy/repository/EventRepository.java
+++ back/src/main/java/com/quest/buddy/repository/EventRepository.java
@@ -3,11 +3,8 @@ package com.quest.buddy.repository;
 import com.quest.buddy.models.Event;
 
 import com.quest.buddy.models.User;
-import com.quest.buddy.models.UserEvent;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
